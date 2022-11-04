435f73fd25b2e9cbe37ed0457eaa6d5751a33278
Vaseliy <bonanava@yandex.ru>
2022-11-04 16:53:24 +0300

    done block1_5

---
+++ 1.5/slider.js
@@ -0,0 +1,16 @@
+// console.log(windowWidth);
+
+// if (windowWidth >= 320 && windowWidth <= 600) {
+
+// const slider = document.querySelector('.swiper');
+
+// const swiper = new Swiper(slider, { 
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         type: 'bullets',
+//       },
+//   });
+
+// }
